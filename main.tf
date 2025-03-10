a6423257f0d82e905341df3cc76c87080769107c
reddomeuk <academy@reddome.org>
2025-03-10 15:49:23 +0000

    First-Branch

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  skip_provider_registration = true
+  skip_provider_registration = false
 }
 
 resource "azurerm_resource_group" "rg" {
